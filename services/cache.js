2fe8a1645591d3ac2e89d943dee25eadad3deb94
gitJank <justinjankiewicz@gmail.com>
2019-06-20 18:30:20 -0500

    adds caching options; adds cleanCahe middleware

--- services/cache.js
+++ services/cache.js
@@ -2,32 +2,48 @@ const mongoose = require("mongoose");
 const redis = require("redis");
 const util = require("util");
 const redisURL = "redis://127.0.0.1:6379";
-
-const client = redis.createClient(redisURL);
-client.get = util.promisify(client.get);
-
 const exec = mongoose.Query.prototype.exec;
+const client = redis.createClient(redisURL);
+client.hget = util.promisify(client.hget);
+
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+
+  return this;
+};
 
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
   const key = JSON.stringify({
     ...this.getQuery(),
     collection: this.mongooseCollection.name
   });
 
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // return cached values if there are any
   if (cacheValue) {
-    console.log("SERVING CACHE");
+    const doc = JSON.parse(cacheValue);
 
-    const doc = new this.model(JSON.parse(cacheValue));
-
-    return doc;
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
   }
 
   //else execute querry, return values, and add to cache
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result));
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
+
   return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
 };
